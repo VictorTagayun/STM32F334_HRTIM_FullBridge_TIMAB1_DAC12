f9f1cf928aade047be8d7255f527ecfe1e076051
victagayun <vic.tagayun@yahoo.com>
2019-06-27 22:46:29 +0800

    Update main.c

--- Src/main.c
+++ Src/main.c
@@ -609,5 +609,3 @@ void assert_failed(char *file, uint32_t line)
   /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
-
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
